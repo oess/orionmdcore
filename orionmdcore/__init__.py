324d00df21ed37b824a46ddc8f9c20eebe1c583a
Gaetano Calabro <gcalabro@eyesopen.com>
2021-08-31 14:04:53 -0600

    Fix pkg version

--- orionmdcore/__init__.py
+++ orionmdcore/__init__.py
@@ -18,7 +18,7 @@
 
 __author__ = "Gaetano Calabro"
 __email__ = "gcalabro@eyesopen.com"
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 __installation__error__ = """
 ERROR: The ORIOMDCORE package requires to work the manual installation of the OpenEye 
