08ac5205f658d74eb47a3a946f7b77cb5b926a7b
Mieczyslaw Torchala <mtorchala@exscientia.co.uk>
2022-02-09 15:25:58 +0000

    updated dependencies

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -3,7 +3,7 @@
 #export PIP_NO_DEPENDENCIES="False"
 #export PIP_NO_INDEX="False"
 
-#pip install OpenEye-orionplatform[artemis]==4.0.0
-#pip install openeye-snowball==0.21.0
+#pip install OpenEye-orionplatform[artemis]==4.2.5
+#pip install openeye-snowball==0.23.1
 
 $PYTHON setup.py install
